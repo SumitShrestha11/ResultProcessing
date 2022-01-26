1723ba98801607ad3cd42cb35121d0c069370cfc
AnishBhetuwal <anishbhetuwal@hotmail.com>
2022-01-26 17:55:21 +0545

    Edit Table

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -3,144 +3,144 @@ import React from 'react'
 const Result = () => {
     return (
         <div className='bg-white rounded-lg shadow-lg'>
-            <table class="border-2 border-black text-center">
+            <table className="border-2 border-black text-center">
                 <tr>
-                    <th class="border-2 border-black border-b-0 w-1/3 underline underline-offset-2 decoration-2" colspan="2">Subjects</th>
-                    <th class="border-2 border-black" colspan="2">Full Marks</th>
-                    <th class="border-2 border-black" colspan="2">Pass Marks</th>
-                    <th class="border-2 border-black" colspan="2">Marks Obtained</th>
-                    <th class="border-2 border-black" rowspan="2">Total</th>
-                    <th class="border-2 border-black" rowspan="2">Remarks</th>
+                    <th className="border-2 border-black border-b-0 w-1/3 underline underline-offset-2 decoration-2" colspan="2">Subjects</th>
+                    <th className="border-2 border-black" colspan="2">Full Marks</th>
+                    <th className="border-2 border-black" colspan="2">Pass Marks</th>
+                    <th className="border-2 border-black" colspan="2">Marks Obtained</th>
+                    <th className="border-2 border-black" rowspan="2">Total</th>
+                    <th className="border-2 border-black" rowspan="2">Remarks</th>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0 border-t-0">Code</th>
-                    <th class="border-2 border-black border-l-0 border-t-0">Title</th>
-                    <th class="border-2 border-black">Asst.</th>
-                    <th class="border-2 border-black">Final</th>
-                    <th class="border-2 border-black">Asst.</th>
-                    <th class="border-2 border-black">Final</th>
-                    <th class="border-2 border-black">Asst.</th>
-                    <th class="border-2 border-black">Final</th>
+                    <th className="border-2 border-black border-r-0 border-t-0">Code</th>
+                    <th className="border-2 border-black border-l-0 border-t-0">Title</th>
+                    <th className="border-2 border-black">Asst.</th>
+                    <th className="border-2 border-black">Final</th>
+                    <th className="border-2 border-black">Asst.</th>
+                    <th className="border-2 border-black">Final</th>
+                    <th className="border-2 border-black">Asst.</th>
+                    <th className="border-2 border-black">Final</th>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
                 <tr>
-                    <th class="border-2 border-black border-r-0">EX451</th>
-                    <th class="border-2 border-black border-l-0">Basic Electronics Engineering</th>
-                    <td class="border-2 border-black">20</td>
-                    <td class="border-2 border-black">80</td>
-                    <td class="border-2 border-black">8</td>
-                    <td class="border-2 border-black">32</td>
-                    <td class="border-2 border-black">18</td>
-                    <td class="border-2 border-black">40</td>
-                    <td class="border-2 border-black">58</td>
-                    <td class="border-2 border-black"></td>
+                    <th className="border-2 border-black border-r-0">EX451</th>
+                    <th className="border-2 border-black border-l-0">Basic Electronics Engineering</th>
+                    <td className="border-2 border-black">20</td>
+                    <td className="border-2 border-black">80</td>
+                    <td className="border-2 border-black">8</td>
+                    <td className="border-2 border-black">32</td>
+                    <td className="border-2 border-black">18</td>
+                    <td className="border-2 border-black">40</td>
+                    <td className="border-2 border-black">58</td>
+                    <td className="border-2 border-black"></td>
                 </tr>
             </table>
         </div>
