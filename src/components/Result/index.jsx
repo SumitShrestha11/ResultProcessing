8107a5d41ac51bbc8c483251288ec8f0efde44f8
AnishBhetuwal <anishbhetuwal@hotmail.com>
2022-01-27 11:00:23 +0545

    Added Information in Table

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -3,14 +3,41 @@ import React from 'react'
 const Result = () => {
     return (
         <div className='bg-white rounded-lg shadow-lg'>
-            <table className="border-2 border-black text-center">
+            <table className="text-left w-full">
                 <tr>
-                    <th className="border-2 border-black border-b-0 w-1/3 underline underline-offset-2 decoration-2" colspan="2">Subjects</th>
-                    <th className="border-2 border-black" colspan="2">Full Marks</th>
-                    <th className="border-2 border-black" colspan="2">Pass Marks</th>
-                    <th className="border-2 border-black" colspan="2">Marks Obtained</th>
-                    <th className="border-2 border-black" rowspan="2">Total</th>
-                    <th className="border-2 border-black" rowspan="2">Remarks</th>
+                    <th className="w-1/6">Name :-</th>
+                    <td className="w-2/6">Ram Kumar</td>
+                    <th className="w-1/6">Exam Roll No :-</th>
+                    <td className="w-2/6">1234</td>
+                </tr>
+                <tr>
+                    <th className="w-1/6">Level :-</th>
+                    <td className="w-2/6">Bachelors' in Engineering</td>
+                    <th className="w-1/6">CRN :-</th>
+                    <td className="w-2/6">2075/BCT/055</td>
+                </tr>
+                <tr>
+                    <th className="w-1/6">Campus :-</th>
+                    <td className="w-2/6">Thapathali Campus</td>
+                    <th className="w-1/6">T.U. Regd. No :-</th>
+                    <td className="w-2/6"></td>
+                </tr>
+                <tr>
+                    <th className="w-1/6"></th>
+                    <td className="w-2/6">Year/Part - I/I</td>
+                    <th className="w-1/6">Programme :-</th>
+                    <td className="w-2/6">Computer Engineering</td>
+                </tr>
+            </table>
+            <br></br>
+            <table className="border-2 border-black text-center w-full">
+                <tr>
+                    <th className="border-2 border-black border-b-0 w-1/3 underline underline-offset-2 decoration-2" colSpan="2">Subjects</th>
+                    <th className="border-2 border-black" colSpan="2">Full Marks</th>
+                    <th className="border-2 border-black" colSpan="2">Pass Marks</th>
+                    <th className="border-2 border-black" colSpan="2">Marks Obtained</th>
+                    <th className="border-2 border-black" rowSpan="2">Total</th>
+                    <th className="border-2 border-black" rowSpan="2">Remarks</th>
                 </tr>
                 <tr>
                     <th className="border-2 border-black border-r-0 border-t-0">Code</th>
@@ -141,6 +168,27 @@ const Result = () => {
                     <td className="border-2 border-black">40</td>
                     <td className="border-2 border-black">58</td>
                     <td className="border-2 border-black"></td>
+                </tr>
+            </table>
+            <br></br>
+            <table className=" border-2 border-black text-left w-full">
+                <tr>
+                    <th className="w-1/6">Marks Enter By :-</th>
+                    <td className="w-2/6"></td>
+                    <th className="w-1/6">Grand Total</th>
+                    <td className="w-1/6">500</td>
+                </tr>
+                <tr>
+                    <th className="w-1/6">Verified By :-</th>
+                    <td className="w-2/6"></td>
+                    <th className="w-1/6">Result</th>
+                    <td className="w-1/6">Passed</td>
+                </tr>
+                <tr>
+                    <th className="w-1/6">Date :-</th>
+                    <td className="w-2/6">10 JAN 2022</td>
+                    <th className="w-1/6"></th>
+                    <td className="w-1/6"></td>
                 </tr>
             </table>
         </div>
