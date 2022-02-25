1150468671501372f046a2ed6d7c9ba5b20d831c
Sumit <sumit.sth54@gmail.com>
2022-02-25 16:59:09 +0545

    updated logo and project name

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       <div className="grid grid-cols-12 gap-5 mx-5 p-10 my-8 bg-mainWindow shadow-lg rounded-lg"> {/*main window*/}
             <div className='col-span-7'>
               <Logo />
-              <span className='ml-5 inline-block'>Organization Name</span>
+              <span className='ml-5 inline-block text-pink-500 font-bold text-3xl'>Data Segmentation and Extraction</span>
               <span className='mt-10 block text-dark-blue font-bold text-3xl'>Result</span>
               <Result isLoading={isLoading} resultData={resultData} setResultData={setResultData}/>
             </div>
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { MdOutlineDocumentScanner } from 'react-icons/md'
 
 export const Logo = () => {
     return (
-        <div className='inline-block text-center align-middle py-6 w-20 h-20 bg-white shadow-lg rounded-full'>
-            logo
+        <div className='inline-block text-center align-middle justify-center py-4 w-20 h-20 bg-white shadow-lg rounded-full'>
+            <MdOutlineDocumentScanner className='text-dark-blue text-5xl ml-4 mb-3' />
         </div> 
         // <div className='top-50 m-10'>
         // </div>
